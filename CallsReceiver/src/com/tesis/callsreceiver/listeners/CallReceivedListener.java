279b150dea641fab77fcc3560f2e9d3eef8cdf70
urik <#mamapapa88>
2014-09-07 20:36:31 -0300

    Deleted a leftover project

--- CallsReceiver/src/com/tesis/callsreceiver/listeners/CallReceivedListener.java
+++
@@ -1,7 +0,0 @@
-package com.tesis.callsreceiver.listeners;
-
-import java.util.Date;
-
-public interface CallReceivedListener {
-    void handleCallHasBeenReceived(String incomingNumber, Date time);
-}
